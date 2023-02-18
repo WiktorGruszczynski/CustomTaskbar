e7c882770d1e1c4d7dc0e830a2853087f5baf4c7
Wiktor <96666718+an-axen@users.noreply.github.com>
2023-02-18 20:21:22 +0100

    Update winapi.py

--- winapi.py
+++ winapi.py
@@ -1,12 +1,29 @@
 import ctypes
-from ctypes.wintypes import RECT
+from ctypes.wintypes import *
 
 
 user32 = ctypes.windll.user32
 gdi32 = ctypes.windll.gdi32
 
-HWND_BOTTOM = 1
+class ACCENTPOLICY(ctypes.Structure):
+    _fields_ = [('nAccentState', DWORD),
+                ('nFlags', DWORD),
+                ('nColor', DWORD),
+                ('nAnimationId', DWORD)]
+
+# Define a structure for the window composition attribute data
+class WINCOMPATTRDATA(ctypes.Structure):
+    _fields_ = [('nAttribute', DWORD),
+                ('pData', ctypes.POINTER(ACCENTPOLICY)),
+                ('ulDataSize', DWORD)]
+
+
 SWP_ASYNCWINDOWPOS = 0x4000
+SWP_NOSIZE = 0x0001
+
+WM_PAINT = 15
+WM_REPAINT = 11
+
 
 def FindWindow(class_name:str=None, window_name:str=None) -> int:
     return user32.FindWindowW(class_name, window_name)
@@ -22,8 +39,8 @@ def GetWindowRect(handle:int):
     return (rect.left, rect.top, rect.right, rect.bottom)
 
 
-def SetWindowPos(hwnd:int, x:int, y:int, width:int, height:int, flags=SWP_ASYNCWINDOWPOS, insert_after=HWND_BOTTOM):
-    user32.SetWindowPos(hwnd, insert_after, x, y, width, height, flags)
+def SetWindowPos(hwnd:int, x:int, y:int, width:int, height:int, flags=SWP_ASYNCWINDOWPOS, insert_after=0):
+    return user32.SetWindowPos(hwnd, insert_after, x, y, width, height, flags)
 
 
 def GetClassName(handle:int) -> str:
@@ -43,14 +60,22 @@ def GetSystemMetrics(index:int) -> int:
     return user32.GetSystemMetrics(index)
 
 
-def GetDC(_NamedFuncPointer=None):
-    return user32.GetDC(_NamedFuncPointer)
+def GetDC(hwnd:int=None) -> int:
+    return user32.GetDC(hwnd)
 
 
-def ReleaseDC(hdc):
-    user32.ReleaseDC(None, hdc)
+def ReleaseDC(hdc:int):
+    return user32.ReleaseDC(None, hdc)
 
 
 def GetDeviceCaps(hdc:int, index:int) -> int:
     return gdi32.GetDeviceCaps(hdc, index)
 
+def SendMessage(hwnd:int, idMessage:int, wparam, lparam):
+    return user32.SendMessageW(hwnd, idMessage, wparam, lparam)
+
+def ShowWindow(hwnd, flags):
+    return user32.ShowWindow(hwnd, flags)
+
+def SetWindowCompositionAttribute(handle:int, wincompattrdata:WINCOMPATTRDATA):
+    user32.SetWindowCompositionAttribute(handle, ctypes.byref(wincompattrdata))
